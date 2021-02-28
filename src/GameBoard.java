972e1533e18e55bbd31857534c117f7b06401d1c
Mausam Shrestha <mashrestha@salud.unm.edu>
2021-02-28 03:28:59 +0000

    GameBoard.java

---
+++ src/GameBoard.java
@@ -0,0 +1,54 @@
+
+/**
+ * GameBoard class represents the actual game board in the domino game, where the player place their dominos
+ * @author: Mausam Shrestha
+ * @date: 02/18/2020
+ * @project: 2 - Domino game
+ * @version: 1
+ * @UNMId: 101865530
+ * @course: CS351
+ */
+
+package sample;
+
+import java.util.LinkedList;
+
+public class GameBoard {
+    /* list of dominos in the game board */
+    private final LinkedList<Domino> board = new LinkedList<>();
+
+    /**
+     * show - prints the list of dominos in board
+     */
+    public void show(){
+        int length;
+        length = this.board.size();
+        for(int i = 0; i < length; i+= 2){
+            System.out.printf("[%d %d] ", board.get(i).getLeft(), board.get(i).getRight());
+        }
+        System.out.println();
+        for(int i = 1; i < length; i+= 2){
+            if(i == 1) System.out.print("  ");
+            System.out.printf("[%d %d] ", board.get(i).getLeft(), board.get(i).getRight());
+        }
+        System.out.print("\n\n");
+    }
+
+    /**
+     * getBoard - gets the dominos on board
+     * @return - the list of dominos on the game board
+     */
+    public LinkedList<Domino> getBoard(){
+        return board;
+    }
+
+    /**
+     * findMatch - checks if given domino is playable
+     * @param domino - the domino we want to check
+     * @return - true if match and false otherwise
+     */
+    public boolean findMatch(Domino domino){
+        return (domino.isMatchLeft(this.getBoard().getFirst())) || (domino.isMatchRight(this.getBoard().getLast()));
+    }
+}
+
